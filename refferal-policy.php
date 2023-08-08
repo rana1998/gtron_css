ae4f702568b039f1ee139219b1c81c9d629dd26d
rana1998 <2015kshitij14@gmail.com>
2023-08-08 17:54:24 +0530

    save

---
+++ refferal-policy.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- Meta -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Discover Gtron, the decentralized MLM Ecosystem powered by blockchain. Sign up now for 500 Gtron absolutely free. Join us in this revolutionary opportunity!">
+    <meta name="keywords" content="Gtron, Blockchain MLM, Decentralized Ecosystem, Crypto MLM, Free Gtron, Blockchain Powered MLM, Decentralized MLM, Gtron Sign Up, MLM Rewards Program">
+	<!-- Page Title -->
+	<title> Referal Policy</title>
+	<meta property="og:locale" content="en_US">
+    <meta property="og:type" content="website" >
+    <meta property="og:title" content="Join Gtron: Blockchain MLM Ecosystem - Get 500 Gtron Free!" >
+    <meta property="og:url" content="https://www.gtron.io" >
+    <meta property="og:image" content="https://www.gtron.io/images/gtron.jpeg" >
+    <meta property="og:image:alt" content="Join Gtron: Blockchain MLM Ecosystem - Get 500 Gtron Free!" > 
+    <meta property="og:description" content="Discover Gtron, the decentralized MLM Ecosystem powered by blockchain. Sign up now for 500 Gtron absolutely free. Join us in this revolutionary opportunity!" >
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:description" content="Discover Gtron, the decentralized MLM Ecosystem powered by blockchain. Sign up now for 500 Gtron absolutely free. Join us in this revolutionary opportunity!">
+    <meta name="twitter:title" content="Join Gtron: Blockchain MLM Ecosystem - Get 500 Gtron Free!">
+    <meta name="twitter:site" content="@gtron_official">
+    <meta name="twitter:image" content="https://www.gtron.io/images/gtron.jpeg">
+    <meta name="twitter:image:alt" content="Join Gtron: Blockchain MLM Ecosystem - Get 500 Gtron Free!">
+    <meta name="twitter:creator" content="@gtron_official">
+    <link rel="canonical" href="https://www.gtron.io" >
+    
+    <meta name="author" content="Lavkush Tari">
+	<!-- Google Fonts css-->
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700,800,900" rel="stylesheet">
+	<!-- Bootstrap css -->
+	<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+	<!-- Font Awesome icon css-->
+	<link href="css/font-awesome.min.css" rel="stylesheet" media="screen">
+	<!-- Main custom css -->
+	<link href="css/custom.css" rel="stylesheet" media="screen">
+	
+    <link rel="stylesheet" type="text/css" href="css/sweetalert2.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-N5QZTXC6');</script>
+	<!-- End Google Tag Manager -->
+    <style>
+       body.swal2-height-auto {
+    height: 100%!important;
+}
+.site-info p {
+    margin-top: 0px;
+}
+
+.comming-soon-info {
+    width: 100%;
+}
+   </style>
+   <script>
+	// if (location.protocol !== 'https:' || location.hostname !== 'www.gtron.io') {
+	// location.href = 'https://www.gtron.io' + location.pathname + location.search;
+	// }
+	</script>
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N5QZTXC6"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+	
+	<!-- Coming Soon Wrapper start -->
+	<div class="comming-soon">
+		<div class="comming-soon-info">
+			<div class="comming-soon-inner">
+				<div>
+					<img style="margin-top:-100px; width: 100px" src="images/title-bg.png" alt="Logo">
+				</div>
+				<!-- Logo end -->
+
+				<div class="site-info">
+				<h1>
+                    <span style="background: #4d3a61; color: white!important; display: inline-block;">Referral Policy</span>
+                </h1>           
+                
+                <p>Referral - Performance Bonus</p>
+
+                <p>Level 1: 5 Direct Referrals</p>
+                <p>Level 2: 25 Referrals</p>
+                <p>Rewards: 100 USDT + 1000 GTC</p>
+
+                <p>Level 3: 125 Referrals</p>
+                <p>Rewards: 200 USDT + 20000 GTC</p>
+
+                <p>Level 1: 10 Direct Referrals</p>
+                <p>Level 2: 100 Referrals</p>
+                <p>Rewards: 200 USDT + 20000 GTC</p>
+
+                <p>Level 3: 1000 Referrals</p>
+                <p>Rewards: 2250 USDT + 275000 GTC</p>
+
+				</div>
+			</div>
+		</div>
+		
+		
+	</div>
+	<!-- Coming Soon Wrapper end -->
+	
+    <!-- Jquery Library File -->
+     <script src="js/sweetalert2.min.js"></script>
+	<script src="js/jquery-1.12.4.min.js"></script>
+	<!-- Timer counter js file -->
+	<script src="js/countdown-timer.js"></script>
+	<!-- SmoothScroll -->
+	<script src="js/SmoothScroll.js"></script>
+    <!-- Bootstrap js file -->
+	<script src="js/bootstrap.min.js"></script>
+    <!-- Main Custom js file -->
+	<script src="js/function.js"></script>
+	<!-- Countdown Script start --> 
+</body>
+</html>
